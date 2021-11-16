aeb9ec980184d4c6f1aee238da6e1e5bbde2fdb9
SoJeongYoony <y940425@gmail.com>
2021-11-16 17:55:23 +0900

    [update 2021-11-16]

---
+++ src/controller/MemberDetailService.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.ModelAndView;
+import service.MemberService;
+
+public class MemberDetailService implements MemberService {
+
+	@Override
+	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
